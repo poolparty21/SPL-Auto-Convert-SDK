f9e6ae22446a52c34869aa098caf542e9894a89b
PoolParty21 <poolparty21@users.noreply.github.com>
2026-06-02 00:58:42 +0800

    fix(build): disable dts generation to bypass workspace resolution

--- packages/listener/tsup.config.ts
+++ packages/listener/tsup.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,
+  format: ['cjs', 'esm'],
+  dts: false,
+  splitting: false,
   sourcemap: true,
   clean: true,
-})
+});
